b7e0f932c33294e6c54693211e0c52b0ae80b3e4
Eric <ericwannn@foxmail.com>
2018-04-20 18:39:24 +0800

    new examples

---
+++ 1_basics/dist_unified/kernel.cu
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#define N 64
+#define TPB 32
+
+float scale(int i, int n)
+{
+    return (float(i) / (n - 1));
+}
+
+__device__
+float distance(float x1, float x2)
+{
+    return sqrt((x1 - x2) * (x1 - x2));
+}
+
+__global__
+void distanceKernel(float *d_out, float *d_in, float ref)
+{
+    const int i = blockIdx.x * blockDim.x + threadIdx.x;
+    const float x = d_in[i];
+    d_out[i] = distance(x, ref);
+    printf("i = %2d: distance from %f to %f is %f.\n", i, ref, x, d_out[i]);
+}
+
+int main()
+{
+    const float ref = 0.5f;
+
+    float *in = 0;
+    float *out = 0;
+
+    cudaMallocManaged(&in, N * sizeof(float));
+    cudaMallocManaged(&out, N * sizeof(float));
+
+    for (int i = 0; i < N; ++i)
+    {
+        in[i] = scale(i, N);
+    }
+
+    distanceKernel<<<N / TPB, TPB>>>(out, in, ref);
+
+    cudaFree(in);
+    cudaFree(out);
+    
+    return 0;
+}
